55dcb78748e5ff9e2aed7b6b0de6bb8fbf1d8f31
sivanzagdon <sivan0252@gmail.com>
2023-06-27 15:15:19 +0300

    update

---
+++ routes.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+
+
+router.get('/Earrings', (req, res) => {
+  res.sendFile('frontEnd/Earrings.html');
+});
+
+router.get('/bracelate', (req, res) => {
+  res.sendFile('frontEnd/bracelate.html');
+});
+
+router.get('/bracelateMem', (req, res) => {
+  res.sendFile('frontEnd/bracelateMen.html');
+});
+
+router.get('/buy_bracelate', (req, res) => {
+  res.sendFile('frontEnd/buy_bracelate.html');
+});
+
+router.get('/buyEarrings', (req, res) => {
+  res.sendFile('frontEnd/buyEarrings.html');
+});
+
+router.get('/buyNecklaces', (req, res) => {
+  res.sendFile('frontEnd/buyNecklaces.html');
+});
+
+router.get('/buyRing', (req, res) => {
+  res.sendFile('frontEnd/buyRing.html');
+});
+
+router.get('/cart', (req, res) => {
+  res.sendFile('frontEnd/cart.html');
+});
+
+router.get('/earMen', (req, res) => {
+  res.sendFile('frontEnd/earMen.html');
+});
+
+router.get('/earMen', (req, res) => {
+  res.sendFile('frontEnd/earMen.html');
+});
+
+router.get('/forgetPassword', (req, res) => {
+  res.sendFile('frontEnd/forgetPassword.html');
+});
+
+router.get('/necklace', (req, res) => {
+  res.sendFile('frontEnd/necklace.html');
+});
+
+
+router.get('/necklaceMen', (req, res) => {
+  res.sendFile('frontEnd/necklaceMen.html');
+});
+
+router.get('/page', (req, res) => {
+  res.sendFile('frontEnd/page.html');
+});
+
+router.get('/ring', (req, res) => {
+  res.sendFile('frontEnd/ring.html');
+});
+
+router.get('/ring1', (req, res) => {
+  res.sendFile('frontEnd/ring1.html');
+});
+
+router.get('/ringformen', (req, res) => {
+  res.sendFile('frontEnd/ringformen.html');
+});
+
+router.get('/s', (req, res) => {
+  res.sendFile('frontEnd/s.html');
+});
+
+
+router.get('/singIn', (req, res) => {
+  res.sendFile('frontEnd/singIn.html');
+});
+
+
+router.get('/singup', (req, res) => {
+  res.sendFile('frontEnd/singup.html');
+});
+
+// הוספת הראוטר לאפליקציה
+app.use("/", router);
+
+// כתיבת הפורט להאזנה
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,29 +1,34 @@
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
+const port = 3000;
 
 // DB connection
-var MONGODB_URL = process.env.MONGODB_URL + process.env.DB_NAME;
-var mongoose = require("mongoose");
+const MONGODB_URL = process.env.MONGODB_URL + process.env.DB_NAME;
 
 mongoose
   .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(
-    () => {
-      console.log("Connected to mongodb successfully");
-    },
-    (err) => {
-      console.error("Cannot connect to mongo server " );
-      console.log(err)
-    }
-  );
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+    // Start the server after successful database connection
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB");
+    console.log(err);
+  });
 
 //const UserModel = require("./models/UserModel");
 
 //var user = new UserModel({
-  //firstName: "hod",
- // lastName: "haim",
- // email: "hod123@gmail.com",
- // password: "1129",
+//firstName: "hod",
+// lastName: "haim",
+// email: "hod123@gmail.com",
+// password: "1129",
 //});
 // user.save()
 
@@ -40,14 +45,13 @@ product.save();
 //const OrdersModel = require("./models/OrdersModel");
 
 //var Orders = new OrdersModel({
-  
-   // UserName:"sivan hod",
-    //OrdersID: "O002",
-   // Totalprice:"10000$",
-   // Orderdate: "11/6/23",
-   // Orderproduct :"jkdsjhjds,dhdhdhd,dhdhdhd",
-   // Totalproduct: "2",
-    
- 
+
+// UserName:"sivan hod",
+//OrdersID: "O002",
+// Totalprice:"10000$",
+// Orderdate: "11/6/23",
+// Orderproduct :"jkdsjhjds,dhdhdhd,dhdhdhd",
+// Totalproduct: "2",
+
 //});
 //Orders.save();
