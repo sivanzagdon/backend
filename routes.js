0e9e9219b3f4d18593d615989ec98edbe7cca0af
sivanzagdon <sivan0252@gmail.com>
2023-06-27 15:45:07 +0300

    up

--- routes.js
+++ routes.js
@@ -1,93 +1,89 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
+const path = require("path");
 
-/////////////////////
-router.get('/Earrings', (req, res) => {
-  res.sendFile('frontEnd/Earrings.html');
+// מיקום התיקייה "frontEnd"
+const staticFilesPath = path.join(__dirname, "frontEnd");
+
+// שימוש ב- express.static עבור התיקייה "frontEnd"
+app.use(express.static(staticFilesPath));
+
+// מגדירים את הראוטים לפי קבצי ה-HTML
+app.get("/Earrings", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "Earrings.html"));
 });
 
-router.get('/bracelate', (req, res) => {
-  res.sendFile('frontEnd/bracelate.html');
+app.get("/bracelate", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "bracelate.html"));
 });
 
-router.get('/bracelateMem', (req, res) => {
-  res.sendFile('frontEnd/bracelateMen.html');
+app.get("/bracelateMem", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "bracelateMen.html"));
 });
 
-router.get('/buy_bracelate', (req, res) => {
-  res.sendFile('frontEnd/buy_bracelate.html');
+app.get("/buy_bracelate", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "buy_bracelate.html"));
 });
 
-router.get('/buyEarrings', (req, res) => {
-  res.sendFile('frontEnd/buyEarrings.html');
+app.get("/buyEarrings", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "buyEarrings.html"));
 });
 
-router.get('/buyNecklaces', (req, res) => {
-  res.sendFile('frontEnd/buyNecklaces.html');
+app.get("/buyNecklaces", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "buyNecklaces.html"));
 });
 
-router.get('/buyRing', (req, res) => {
-  res.sendFile('frontEnd/buyRing.html');
+app.get("/buyRing", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "buyRing.html"));
 });
 
-router.get('/cart', (req, res) => {
-  res.sendFile('frontEnd/cart.html');
+app.get("/cart", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "cart.html"));
 });
 
-router.get('/earMen', (req, res) => {
-  res.sendFile('frontEnd/earMen.html');
+app.get("/earMen", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "earMen.html"));
 });
 
-router.get('/earMen', (req, res) => {
-  res.sendFile('frontEnd/earMen.html');
+app.get("/forgetPassword", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "forgetPassword.html"));
 });
 
-router.get('/forgetPassword', (req, res) => {
-  res.sendFile('frontEnd/forgetPassword.html');
+app.get("/necklace", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "necklace.html"));
 });
 
-router.get('/necklace', (req, res) => {
-  res.sendFile('frontEnd/necklace.html');
+app.get("/necklaceMen", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "necklaceMen.html"));
 });
 
-
-router.get('/necklaceMen', (req, res) => {
-  res.sendFile('frontEnd/necklaceMen.html');
+app.get("/page", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "page.html"));
 });
 
-router.get('/page', (req, res) => {
-  res.sendFile('frontEnd/page.html');
+app.get("/ring", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "ring.html"));
 });
 
-router.get('/ring', (req, res) => {
-  res.sendFile('frontEnd/ring.html');
+app.get("/ring1", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "ring1.html"));
 });
 
-router.get('/ring1', (req, res) => {
-  res.sendFile('frontEnd/ring1.html');
+app.get("/ringformen", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "ringformen.html"));
 });
 
-router.get('/ringformen', (req, res) => {
-  res.sendFile('frontEnd/ringformen.html');
+app.get("/s", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "s.html"));
 });
 
-router.get('/s', (req, res) => {
-  res.sendFile('frontEnd/s.html');
+app.get("/singIn", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "singIn.html"));
 });
 
-
-router.get('/singIn', (req, res) => {
-  res.sendFile('frontEnd/singIn.html');
+app.get("/singup", (req, res) => {
+  res.sendFile(path.join(staticFilesPath, "singup.html"));
 });
-
-
-router.get('/singup', (req, res) => {
-  res.sendFile('frontEnd/singup.html');
-});
-
-// הוספת הראוטר לאפליקציה
-app.use("/", router);
 
 // כתיבת הפורט להאזנה
 const port = 3000;
