914d1ef1aa5487e6d885de0ff29bfcb86bb42487
sivanzagdon <sivan0252@gmail.com>
2023-08-04 12:05:49 +0300

    Check why the git push stack

--- index.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const {default: mongoose }=require("mongoose");
-const  product= require("./models/ProductModel");
-const app=express();
-
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
-
-mongoose.connect('');
